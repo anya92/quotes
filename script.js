516a93961bce5420817ec083c07d5788fe952999
anya92 <aniabrajtkopf@gmail.com>
2016-12-13 18:52:55 +0100

    new project

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  var randomNum;
+  var randomQuote;
+  var randomAuthor;
+  var url = 'quotes.json';
+
+  function getQuote() {
+    $.getJSON(url, function(data) {
+      randomNum = Math.floor((Math.random() * data.quotes.length));
+      randomQuote = data.quotes[randomNum];
+      randomAuthor = data.authors[randomNum];
+      $("#quote").text(randomQuote);
+      $("#author").text(randomAuthor);
+      var text = randomQuote + ' - ' + randomAuthor;
+      $('.twitter').attr('href', "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text));
+      console.log(randomQuote + randomAuthor);
+    });
+  }
+  getQuote();
+  $('.get_quote').on('click', function(event) {
+      event.preventDefault();
+      getQuote();
+  });
+});
