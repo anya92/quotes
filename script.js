764234e9adf2f51a4a73c65395edb9871518e004
anya92 <aniabrajtkopf@gmail.com>
2016-12-13 20:10:21 +0100

    Poprawiony wygląd i cytaty po polsku

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
-  var randomNum;
-  var randomQuote;
-  var randomAuthor;
-  var url = 'quotes.json';
+    var randomNum;
+    var randomQuote;
+    var randomAuthor;
+    var url = 'quotes.json';
 
-  function getQuote() {
-    $.getJSON(url, function(data) {
-      randomNum = Math.floor((Math.random() * data.quotes.length));
-      randomQuote = data.quotes[randomNum];
-      randomAuthor = data.authors[randomNum];
-      $("#quote").text(randomQuote);
-      $("#author").text(randomAuthor);
-      var text = randomQuote + ' - ' + randomAuthor;
-      $('.twitter').attr('href', "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text));
-      console.log(randomQuote + randomAuthor);
+    function getQuote() {
+        $.getJSON(url, function(data) {
+            randomNum = Math.floor((Math.random() * data.quotes.length));
+            randomQuote = data.quotes[randomNum];
+            randomAuthor = data.authors[randomNum];
+            $("#quote").text('"' + randomQuote + '"');
+            $("#author").text(randomAuthor);
+            var text = randomQuote + ' - ' + randomAuthor;
+            $('.twitter').attr('href', "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text));
+        });
+    }
+    getQuote();
+    $('.get_quote').on('click', function(event) {
+        event.preventDefault();
+        getQuote();
     });
-  }
-  getQuote();
-  $('.get_quote').on('click', function(event) {
-      event.preventDefault();
-      getQuote();
-  });
 });
