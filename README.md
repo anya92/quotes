da0146864b3abbef56c38f73d889327e9aab68b5
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-01-12 18:38:08 +0100

    poprawki

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-quote-machine
 Free Code Camp Project
+
+https://anya92.github.io/quotes/
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var randomNum;
     var randomQuote;
     var randomAuthor;
     var url = 'quotes.json';
 
     function getQuote() {
-        $.getJSON(url, function(data) {
+        $.getJSON(url, function (data) {
             randomNum = Math.floor((Math.random() * data.quotes.length));
             randomQuote = data.quotes[randomNum];
             randomAuthor = data.authors[randomNum];
             $("#quote").text('"' + randomQuote + '"');
             $("#author").text(randomAuthor);
-            var text = randomQuote + ' - ' + randomAuthor;
+            var text = '"' + randomQuote + '"' + ' - ' + randomAuthor;
             $('.twitter').attr('href', "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text));
         });
     }
     getQuote();
-    $('.get_quote').on('click', function(event) {
+    $('.get_quote').on('click', function (event) {
         event.preventDefault();
         getQuote();
     });
